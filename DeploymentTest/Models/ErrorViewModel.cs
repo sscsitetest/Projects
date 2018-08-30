0dae02af1f6f91f6e79eca309ef85a06923a41d1
ItactDev <itactdev1@itactsolutions.com>
2018-08-30 17:04:15 +0530

    Intial Commint

---
+++ DeploymentTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeploymentTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
